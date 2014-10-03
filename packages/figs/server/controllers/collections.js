ab6048720db3da5d9803e7bf5a946041ff4bb24e
spiddy <d.kapanidis@gmail.com>
2014-10-03 20:13:33 +0200

    Collections are now shown correctly on /v1/c/:user/:name

--- packages/figs/server/controllers/collections.js
+++ packages/figs/server/controllers/collections.js
@@ -5,6 +5,7 @@
  */
 var mongoose = require('mongoose'),
   Fig = mongoose.model('Fig'),
+  Repo = mongoose.model('Repo'),
   User = mongoose.model('User');
 
 /**
@@ -28,33 +29,37 @@ exports.showUserFigs = function(req, res) {
  * Show a fig by user and name
  */
 exports.show = function(req, res) {
+  console.log('hi' + req.params.user + req.params.name);
   User.findOne({username: req.params.user}, function(err, user) {
     var userId = user? user._id:'';
-    Fig.findOne({user: userId, name: req.params.name}).populate('repo', 'name description').populate('user', 'username').exec(function(err, fig) {
-    if (err || !fig) {
-      return res.json(500, {
-        error: 'Cannot list the fig'
+    Repo.findOne({name: req.params.name}, function(err, repo){
+      var repoId = repo? repo._id:'';
+      Fig.findOne({user: userId, repo: repoId}).populate('repo', 'name description').populate('user', 'username').exec(function(err, fig) {
+      if (err || !fig) {
+        return res.json(500, {
+          error: 'Cannot list the fig'
+        });
+      }
+      res.json(fig);
       });
-    }
-    res.json(fig);
     });
   });
 };
 
-/**
- * Show a fig by user, name and version
- */
-exports.show = function(req, res) {
-  User.findOne({username: req.params.user}, function(err, user) {
-    var userId = user? user._id:'';
-    Fig.findOne({user: userId, name: req.params.name}).populate('user', 'username').exec(function(err, fig) {
-    if (err || !fig) {
-      return res.json(500, {
-        error: 'Cannot list the fig'
-      });
-    }
-    res.json(fig);
-    });
-  });
-};
+// /**
+//  * Show a fig by user, name and version
+//  */
+// exports.show = function(req, res) {
+//   User.findOne({username: req.params.user}, function(err, user) {
+//     var userId = user? user._id:'';
+//     Fig.findOne({user: userId, name: req.params.name}).populate('user', 'username').exec(function(err, fig) {
+//     if (err || !fig) {
+//       return res.json(500, {
+//         error: 'Cannot list the fig'
+//       });
+//     }
+//     res.json(fig);
+//     });
+//   });
+// };
 
